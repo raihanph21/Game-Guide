4c78b2b386bcd770a3ec36e9dbc71e392c974f2d
raihanph21 <m.rhn.ph@gmail.com>
2023-06-14 15:16:18 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ambil elemen-elemen yang diperlukan
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navUl');
+
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+    navbarMenu.style.display = navbarMenu.classList.contains('active') ? 'block' : 'none';
+  });
