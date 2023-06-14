b04ced04746d6656b82f92d6386b393a7c6902fa
raihanph21 <m.rhn.ph@gmail.com>
2023-06-14 15:20:14 +0700

    mengganti js dikit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Ambil elemen-elemen yang diperlukan
+// Ambil elemen-elemen 
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navUl');
 
-navbarToggle.addEventListener('click', () => {
+navbarToggle.addEventListener('onclick', function() {
     navbarMenu.classList.toggle('active');
     navbarMenu.style.display = navbarMenu.classList.contains('active') ? 'block' : 'none';
   });
